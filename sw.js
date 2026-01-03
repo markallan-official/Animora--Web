e72b33c6fd7fed6f53989722c2b8253d407b80c9
markallan-official <markmallan01@gmail.com>
2026-01-03 17:53:10 +0300

    Fix cache and service worker serving old versions

--- sw.js
+++ sw.js
@@ -7,18 +7,14 @@ const FILES_TO_CACHE = [
   "/script.js"
 ];
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", () => {
+  clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
